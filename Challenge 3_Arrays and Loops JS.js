3cdb71a22aa089d0fc796b790ee0ed3c6d693b24
roshjoe <155600170+roshjoe@users.noreply.github.com>
2024-05-08 11:07:58 -0600

    Add files via upload

---
+++ Challenge 3_Arrays and Loops JS.js
@@ -0,0 +1,12 @@
+// Arrays and Loops
+//Step 1
+numberList = [];
+
+//Step 2 - Add numbers from 0 to 5 into the array numberList and print them
+for (i = 0; i <= 5; i++)
+    numberList.push(i);
+console.log(numberList);
+
+// Step 3 Remove the last item in the array and print the new array
+numberList.pop(i);
+console.log(numberList);
